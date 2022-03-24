957741a4a0991d48afde4d0bd1f377bdf63cb8dd
Fabian Silva <silvafabian74@yahoo.com>
2022-03-24 18:21:07 -0400

    stub up controller functions for restrooms

---
+++ controllers/restrooms.js
@@ -0,0 +1,48 @@
+import { Restroom } from '../models/restroom'
+
+function index (req, res) {
+  Restroom.find({})
+  .then(restooms => {
+    res.json(restrooms)
+  })
+  .catch(err => {
+    res.json
+  })
+}
+
+function create (req, res) {
+  req.body.author = req.user.profile
+  Restroom.create(req.body)
+  .then(restroom => {
+    restroom.populate('author')
+  })
+  .catch(err => {
+    res.json(err)
+  })
+}
+
+function deleteRestroom(req, res) {
+  Restroom.findByIdAndDelete(req.params.id)
+  .then(restroom => res.json(restroom))
+  .catch(err => res.json(err))
+}
+
+function update(req, res) {
+  Restroom.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(restroom => res.json(restroom))
+  .catch(err => res.json(err))
+}
+
+function show(req, res) {
+  Restroom.findById(req.params.id)
+  .then(restroom => res.json(restroom))
+  .catch(err => res.json(err))
+}
+
+export {
+  index,
+  create,
+  deleteRestroom as  delete,
+  update,
+  show,
+}
