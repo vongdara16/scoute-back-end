e30fbf78566db71d733a5f4fe9c0427c2313a515
Fabian Silva <silvafabian74@yahoo.com>
2022-03-24 18:56:29 -0400

    stub up functions for parking controller

---
+++ controllers/parkinglots.js
@@ -0,0 +1,48 @@
+import { Parking } from '../models/parking.js'
+
+function index (req, res) {
+  Parking.find({})
+  .then(parkinglot => {
+    res.json(parkinglot)
+  })
+  .catch(err => {
+    res.json
+  })
+}
+
+function create (req, res) {
+  req.body.author = req.user.profile
+  Parking.create(req.body)
+  .then(parkinglot => {
+    parkinglot.populate('author')
+  })
+  .catch(err => {
+    res.json(err)
+  })
+}
+
+function deleteParking(req, res) {
+  Parking.findByIdAndDelete(req.params.id)
+  .then(parkinglot => res.json(parkinglot))
+  .catch(err => res.json(err))
+}
+
+function update(req, res) {
+  Parking.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(parkinglot => res.json(parkinglot))
+  .catch(err => res.json(err))
+}
+
+function show(req, res) {
+  Parking.findById(req.params.id)
+  .then(parkinglot => res.json(parkinglot))
+  .catch(err => res.json(err))
+}
+
+export {
+  index,
+  create,
+  deleteParking as parking,
+  update,
+  show
+}
