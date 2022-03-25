e862b9f43ef4205046fda09c2a7a97e023781a66
Fabian Silva <silvafabian74@yahoo.com>
2022-03-25 10:35:11 -0400

    fix review route functions

--- routes/reviews.js
+++ routes/reviews.js
@@ -15,21 +15,21 @@ router.get('/restaurants/:id/reviews', checkAuth, reviewsCtrl.index)
 router.get('/restrooms/:id/reviews', checkAuth, reviewsCtrl.index)
 router.get('/parkinglots/:id/reviews', checkAuth, reviewsCtrl.index)
 
-router.get('/restaurants/:id/reviews/:id', checkAuth, reviewsCtrl.show)
-router.get('/restrooms/:id/reviews/:id', checkAuth, reviewsCtrl.show)
-router.get('/parkinglots/:id/reviews/:id', checkAuth, reviewsCtrl.show)
+router.get('/restaurants/:id/reviews/:reviewId', checkAuth, reviewsCtrl.show)
+router.get('/restrooms/:id/reviews/:reviewId', checkAuth, reviewsCtrl.show)
+router.get('/parkinglots/:id/reviews/:reviewId', checkAuth, reviewsCtrl.show)
 
 router.post('/restaurants/:id/reviews', checkAuth, reviewsCtrl.create)
 router.post('/restrooms/:id/reviews', checkAuth, reviewsCtrl.create)
 router.post('/parkinglots/:id/reviews', checkAuth, reviewsCtrl.create)
 
-router.put('/restaurants/:id/reviews/:id', checkAuth, reviewsCtrl.update)
-router.put('/restrooms/:id/reviews/:id', checkAuth, reviewsCtrl.update)
-router.put('/parkinglots/:id/reviews/:id', checkAuth, reviewsCtrl.update)
+router.put('/restaurants/:id/reviews/:reviewId', checkAuth, reviewsCtrl.update)
+router.put('/restrooms/:id/reviews/:reviewId', checkAuth, reviewsCtrl.update)
+router.put('/parkinglots/:id/reviews/:reviewId', checkAuth, reviewsCtrl.update)
 
-router.delete('/restaurants/:id/reviews/:id', checkAuth, reviewsCtrl.delete)
-router.delete('/restrooms/:id/reviews/:id', checkAuth, reviewsCtrl.delete)
-router.delete('/parkinglots/:id/reviews/:id', checkAuth, reviewsCtrl.delete)
+router.delete('/restaurants/:id/reviews/:reviewId', checkAuth, reviewsCtrl.delete)
+router.delete('/restrooms/:id/reviews/:reviewId', checkAuth, reviewsCtrl.delete)
+router.delete('/parkinglots/:id/reviews/:reviewId', checkAuth, reviewsCtrl.delete)
 
 export {
   router
