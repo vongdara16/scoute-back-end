f22dd30495c2f88b951c9c9240a1af161ed54abe
Fabian Silva <silvafabian74@yahoo.com>
2022-03-25 11:51:00 -0400

    update and change functionality to review routes and controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -11,17 +11,9 @@ const router = Router()
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
 
-router.get('/restaurants/:id/reviews', checkAuth, reviewsCtrl.index)
-router.get('/restrooms/:id/reviews', checkAuth, reviewsCtrl.index)
-router.get('/parkinglots/:id/reviews', checkAuth, reviewsCtrl.index)
-
-router.get('/restaurants/:id/reviews/:reviewId', checkAuth, reviewsCtrl.show)
-router.get('/restrooms/:id/reviews/:reviewId', checkAuth, reviewsCtrl.show)
-router.get('/parkinglots/:id/reviews/:reviewId', checkAuth, reviewsCtrl.show)
-
-router.post('/restaurants/:id/reviews', checkAuth, reviewsCtrl.create)
-router.post('/restrooms/:id/reviews', checkAuth, reviewsCtrl.create)
-router.post('/parkinglots/:id/reviews', checkAuth, reviewsCtrl.create)
+router.post('/restaurants/:id', checkAuth, reviewsCtrl.create)
+router.post('/restrooms/:id', checkAuth, reviewsCtrl.create)
+router.post('/parkinglots/:id', checkAuth, reviewsCtrl.create)
 
 router.put('/restaurants/:id/reviews/:reviewId', checkAuth, reviewsCtrl.update)
 router.put('/restrooms/:id/reviews/:reviewId', checkAuth, reviewsCtrl.update)
